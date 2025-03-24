998bcdeef84dc62e66669019e34c0ae9a532ba16
bax082024 <bax082024@gmail.com>
2025-03-24 08:46:15 +0100

    added playtone method

--- tone_generator.py
+++ tone_generator.py
@@ -1,2 +1,10 @@
 import numpy as np
 import sounddevice as sd
+
+def play_tone(frequency, duration=2, sample_rate=44100):
+    t = np.linspace(0, duration, int(sample_rate * duration), endpoint=False)
+    waveform = 0.5 * np.sin(2 * np.pi * frequency * t)
+    sd.play(waveform, sample_rate)
+    sd.wait()
+
+    
